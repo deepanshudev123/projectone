f3749f0ca5bbd9020aa1fa0cf6460768e7806169
deepanshudev123 <dipanshu.307696@gmail.com>
2024-07-24 15:46:00 +0530

    Add files via upload

---
+++ Projrct.js
@@ -0,0 +1,67 @@
+function Project(){
+    return(
+
+        <div className="rohit">
+            <div className="vanshu">
+            <div className="nipu1"></div>
+            <h1 className="nipu">TESTIMONIAL SLIDER</h1>
+            <div className="nipu2"></div>
+            </div>
+            <div className="akash">
+                <div className="sagun">
+                    <div className="prayia">
+                    <img src="https://www.trueanthem.com/wp-content/uploads/2023/05/human-and-AI-collaboration-scaled.jpeg"className="ranu"></img>
+                    <h1 className="rishu">PERSON'S NAME</h1>
+                    <h2>Designetion Goes-Here</h2>
+                    <h4>Lotem Ipsum is simple dummy text of the printing<br></br>
+                         and typesetting industry Lorem Ipusem has been<br></br> the industry's dummy text ever since</h4>
+                         <div className="nilu">
+                         <i class="fa-brands fa-facebook"></i>
+                             <i class="fa-brands fa-youtube"></i>
+                             <i class="fa-brands fa-instagram"></i>
+                             <i class="fa-brands fa-whatsapp"></i>
+                             </div>
+                    </div>
+                </div>
+                <div className="kishan">
+                    <div className="puchu">
+                        <img src="https://i.pinimg.com/originals/e5/c8/03/e5c803a1f390a8bfb5f45abd8a055ba5.jpg" className="rohan"></img>
+                        <h1 className="rishu">PERSON'S NAME</h1>
+                        <h2>Designetion Goes-Here</h2>
+                        <h4>Lotem Ipsum is simple dummy text of the printing<br></br> 
+                        and typesetting industry Lorem Ipusem has been <br></br>the industry's dummy text ever since</h4>
+                        <div className="nilu">
+                        <i class="fa-brands fa-facebook"></i>
+                             <i class="fa-brands fa-youtube"></i>
+                             <i class="fa-brands fa-instagram"></i>
+                             <i class="fa-brands fa-whatsapp"></i>
+                             </div>
+                    </div>
+                </div>
+                <div className="anish">
+                    <div className="radha">
+                        <img src="https://www.eficiens.com/assets/uploads/2023/03/paris-blockchain-week-2023.jpg" className="gopi"></img>
+                        <h1 className="rishu">PERSON'S NAME</h1>
+                        <h2>Designetion Goes-Here</h2>
+                        <h4>Lotem Ipsum is simple dummy text of the printing<br></br> 
+                        and typesetting industry Lorem Ipusem has been<br></br> the industry's dummy text ever since</h4>
+                        <div className="nilu">
+                        <i class="fa-brands fa-facebook"></i>
+                             <i class="fa-brands fa-youtube"></i>
+                             <i class="fa-brands fa-instagram"></i>
+                             <i class="fa-brands fa-whatsapp"></i>
+                             </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+
+
+
+        </div>
+    )
+}
+export default Project;
